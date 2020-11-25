56053d64d7050a942a567833135a5a9233a68904
Maximiliano Burgos <maxib2007@gmail.com>
2020-11-25 12:25:27 -0300

    arrancando proyecto en consola

---
+++ bin/DartRolGame.dart
@@ -0,0 +1,5 @@
+import 'package:DartRolGame/DartRolGame.dart' as DartRolGame;
+
+void main(List<String> arguments) {
+  print('Hello world: ${DartRolGame.calculate()}!');
+}
---
+++ test/DartRolGame_test.dart
@@ -0,0 +1,8 @@
+import 'package:DartRolGame/DartRolGame.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
